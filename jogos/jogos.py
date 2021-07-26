35d82a5b63f546a2939ac955d27c6622de80400d
Marcelo Firmino <mfirmino@gmail.com>
2021-07-26 13:27:10 -0300

    Aula 8 parte 1

---
+++ jogos/jogos.py
@@ -0,0 +1,16 @@
+import forca
+import adivinhacao_com_for
+
+print("*********************************")
+print("*******Escolha o seu jogo!*******")
+print("*********************************")
+
+print("(1) Forca - (2) Adivinhação")
+jogo = int(input("Qual será o jogo: "))
+
+if jogo == 1:
+    print("Jogando forca")
+    forca.jogar()
+elif jogo ==2:
+    print("Jogando adivinhação")
+    adivinhacao_com_for.jogar()
