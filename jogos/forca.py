8016d3b7b9ced20a0365588e0717dcb06c636a90
Marcelo Firmino <mfirmino@gmail.com>
2021-08-29 13:40:57 -0300

    Conclusão do jogo da forca na primeira versão.

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,27 +4,41 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    qtd_tentativas = 6
+    qtd_erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra ?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print(f"encontrei a letra {chute} na posição {index}")
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    print(f"encontrei a letra {chute} na posição {index}")
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            qtd_erros += 1
+
+        enforcou = qtd_erros == qtd_tentativas
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-        print(f"Ainda falta(m) {letras_acertadas.count('_')} letra(s) para completar a palavra secreta")
+
+        if(acertou):
+            print("Parabéns !!! Você Ganhou !!!!")
+            print(f"A palavra secreta era: {palavra_secreta.capitalize()}")
+        else:
+            print(f"Ainda falta(m) {letras_acertadas.count('_')} letra(s) para completar a palavra secreta")
+            print(f"Quantidade de tentativas restantes: {qtd_tentativas - qtd_erros}")
 
     print("Fim de jogo.")
 
